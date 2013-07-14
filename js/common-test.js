9fed581ea2fbe855e7701780bdfd45c2747b73c2
recle <recle.strife@gmail.com>
2013-07-14 23:03:55 +0800

    Add getToken utilities.

--- js/common-test.js
+++ js/common-test.js
@@ -1,16 +1,44 @@
+/*
+	Unit Test - common-test.js
+	Test js/common.js functions
+*/
+
 module("Common utility test - js/common.js");
 
+/*
+	Test - fmt.getSigniture
+*/
 test("Test get API signiture - fmt.getSigniture", function() {
 
-	var secret = "65a231ec9c93e197";
+    equal(ftm.getSigniture(null), "3fb967f893daba495c27aa94bd47ce45", "Null parameters");
+    equal(ftm.getSigniture({}), "3fb967f893daba495c27aa94bd47ce45", "Empty parameters");
+    equal(ftm.getSigniture({api_key: "487xyz", method: "getToken"}), "aac40f6ba3f465d6dc2231cfebe01c88", "Normal parameters");
+    equal(ftm.getSigniture({method: "getToken", api_key: "487xyz"}), "aac40f6ba3f465d6dc2231cfebe01c88", "Normal parameters no order");
+    equal(ftm.getSigniture({c: "d", a: "b", j: "k"}), "28305a27585892f711c973dd37ff1b37", "Normal parameters no order 2");
 
-	equal(ftm.getSigniture({name: "1"}, ""), "", "Empty secret");
-	equal(ftm.getSigniture({name: "code"}, undefined), "", "Undefined secret");
-	equal(ftm.getSigniture({name: "code"}, null), "", "Null secret");
-	equal(ftm.getSigniture({name: "code"}, {}), "3f1618b22eb967eb3b2639c7bf6ee423", "Empty object secret");
-	equal(ftm.getSigniture(null, secret), "3fb967f893daba495c27aa94bd47ce45", "Null parameters");
-	equal(ftm.getSigniture({}, secret), "3fb967f893daba495c27aa94bd47ce45", "Empty parameters");
-	equal(ftm.getSigniture({api_key: "487xyz", method: "getToken"}, secret), "aac40f6ba3f465d6dc2231cfebe01c88", "Normal parameters");
-	equal(ftm.getSigniture({method: "getToken", api_key: "487xyz"}, secret), "aac40f6ba3f465d6dc2231cfebe01c88", "Normal parameters no order");
-	equal(ftm.getSigniture({c: "d", a: "b", j: "k"}, secret), "28305a27585892f711c973dd37ff1b37", "Normal parameters no order 2");
-})
+});
+
+/*
+	Test - fmt.getUrl
+*/
+test("Test get URL - fmt.getUrl", function() {
+
+	equal(ftm.getUrl("http://example.com/", {simple: "true", count: 1}, false, false), "http://example.com/?simple=true&count=1", "No API key, no signiture");
+	equal(ftm.getUrl("http://example.com/", {}, false, false), "http://example.com/", "No params");
+	equal(ftm.getUrl("http://example.com/", {}, true, false), "http://example.com/?api_key=12614411f8275adf7f290ed73088dfae", "No signiture");
+	equal(ftm.getUrl("http://example.com/", {}, true, true), "http://example.com/?api_key=12614411f8275adf7f290ed73088dfae&api_sig=72d317a4ad47f685e84b3f2464206e09", "Simple signiture");
+    equal(ftm.getUrl(ftm.authUrl, {perms: "delete"}, true, true), ftm.authUrl + "?perms=delete&api_key=12614411f8275adf7f290ed73088dfae&api_sig=ff34a67f4b60f43578717a70be0e910f", "Auth URL");
+    equal(ftm.getUrl(ftm.apiUrl, {method: "rtm.auth.getToken", frob: "96286411c93e908eb75a35e75f8056fac19f2f7b"}, true, true), 
+    	ftm.apiUrl + "?method=rtm.auth.getToken&frob=96286411c93e908eb75a35e75f8056fac19f2f7b&api_key=12614411f8275adf7f290ed73088dfae&api_sig=1272aabf3f126ac53219edbe6ed31fc1", "API URL");
+
+});
+
+/*
+
+*/
+test("Test login", function() {
+
+	ftm.getToken();
+	ok(true, "");
+
+});
--- js/common.js
+++ js/common.js
@@ -1,21 +1,120 @@
 /*
-	requires - underscore.js
-	         - md5.js
+    Common utilities
+    Requires: underscore.js, md5.js
 */
 
-
+/*
+    Namespace
+*/
 var ftm = ftm || {};
 
-ftm.getSigniture = function(params, secret) {
 
-	if (!secret)
-		return "";
+/*
+    Constants
+*/
 
-	var allArgs = "";
-	var args = _.keys(params || {}).sort();
-	_.each(args, function(e, i, arr) {
-		allArgs += (e + params[e]);
-	})
+ftm.authUrl = "http://www.rememberthemilk.com/services/auth/";
+ftm.apiUrl = "https://api.rememberthemilk.com/services/rest/";
 
-	return md5(secret.toString() + allArgs);
+ftm.apiKey = "12614411f8275adf7f290ed73088dfae";
+ftm.secret = "65a231ec9c93e197";
+
+/* 
+    Gets the RTM API signiture
+*/
+ftm.getSigniture = function(params) {
+
+    var allArgs = "";
+    var args = _.keys(params || {}).sort();
+    _.each(args, function(e, i) {
+        allArgs += (e + params[e]);
+    });
+
+    return md5(ftm.secret.toString() + allArgs);
+}
+
+/*
+    Builds request URL  
+*/
+ftm.getUrl = function (base, params, withApiKey, withApiSig) {
+
+    if (!base)
+        return "";
+
+    params = params || {};
+    if (withApiKey)
+        params.api_key = ftm.apiKey;
+
+    var url = base;
+    var start = true;
+    _.each(params, function(v, k) {
+        url += start ? ("?" + k + "=" + v) : ("&" + k + "=" + v);
+        start = start ? false : start;
+    });
+
+    if (withApiSig) {
+        var sig = ftm.getSigniture(params);
+        url += _.isEmpty(params) ? ("?api_sig=" + sig) : ("&api_sig=" + sig);
+    }
+
+    return url;
+}
+
+ftm.getToken = function (succeeded) {
+
+    ftm.token = sessionStorage.getItem("ftm.token");
+    if (ftm.token && ftm.token.length > 0)
+        return true;
+
+    var frob = sessionStorage.getItem("ftm.frob");
+    // alert(frob);
+    if (!frob || frob.length == 0) {
+
+        var getFrobUrl = ftm.getUrl(ftm.apiUrl, {method: "rtm.auth.getFrob"}, true, true);
+        console.log(getFrobUrl);
+
+        $.get(getFrobUrl, function(data) {
+            
+            // Get frob
+            if ($("rsp", data).attr("stat") == "ok") {
+                frob = $("frob", data).text();
+                console.log(frob);
+                sessionStorage.setItem("ftm.frob", frob);
+
+                // Authorize
+                // alert("go: " + frob);
+                window.location = ftm.getUrl(ftm.authUrl, {perms: "delete", "frob": frob}, true, true);
+            }
+        });
+
+
+    }
+
+    else {
+
+        // alert("godeep: " + ftm.token);
+        $.get(ftm.getUrl(ftm.apiUrl, {method: "rtm.auth.getToken", "frob": frob}, true, true), function(data) {
+
+            if ($("rsp", data).attr("stat") == "ok") {
+                ftm.token = $("token", data).text();
+                sessionStorage.setItem("ftm.token", ftm.token);
+                var $user = $("user", data);
+                ftm.user = $user.attr("username");
+                ftm.userName = $user.attr("fullname");
+                ftm.userId = $user.attr("id");
+                sessionStorage.setItem("ftm.user", ftm.user);
+                sessionStorage.setItem("ftm.userName", ftm.userName);
+                sessionStorage.setItem("ftm.userId", ftm.userId);
+                console.log("token: " + ftm.token);
+                // alert("token: "+ ftm.token);
+
+                if (succeeded)
+                    succeeded.apply(this);
+            }
+
+        });
+
+    }
+
+    return false;
 }
